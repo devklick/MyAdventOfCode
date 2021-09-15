9164e30f0efe2ac2fc73044e4aed6b6e9e7554d7
devklick <devklick@gmail.com>
2021-09-15 17:25:01 +0100

    Added solution that consists of one Xunit project, along with one test class containing answers to day 1 of year 2015's challenges

---
+++ MyAdventOfCode/Extensions/TestOutputHelperExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Xunit.Abstractions;
+
+namespace MyAdventOfCode.Extensions
+{
+    public static class TestOutputHelperExtensions
+    {
+        public static void WriteLine(this ITestOutputHelper helper, object valueToOutput)
+            => helper.WriteLine(valueToOutput?.ToString());
+    }
+}
