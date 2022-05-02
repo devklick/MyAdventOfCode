8d8fc9c9e4df994fb315cb6b9fadf964b56fe07f
devklick <devklick@gmail.com>
2022-05-02 23:40:22 +0100

    Removed debug code

--- MyAdventOfCode/Y2021/d3.js
+++ MyAdventOfCode/Y2021/d3.js
@@ -73,8 +73,6 @@ const getGammaRate = (binaryValues) => getXRate(binaryValues, "gamma");
  */
 const getEpsilonRate = (binaryValues) => getXRate(binaryValues, "epsilon");
 
-const binaryValues = ['00100','11110','10110','10111','10101','01111','00111','11100','10000','11001','00010','01010'];
-
 const run = async () => {
   const data = await (await getInput(3)).split('\n');
   const epsilon = getEpsilonRate(data);
