e523bb6da1a649b2dd12f05b661f0cf7bf6fbfc7
devklick <devklick@gmail.com>
2022-12-10 14:36:15 +0000

    Answers for Y2022 D08

---
+++ MyAdventOfCode/Y2022/D08/Solution.cs
@@ -0,0 +1,224 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using MyAdventOfCode.Common;
+
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MyAdventOfCode.Y2022.D08;
+
+public class Solution : TestBase
+{
+    public Solution(ITestOutputHelper output) : base(output)
+    { }
+
+    [Fact]
+    public override async Task Part1_Example()
+        => await Invoke_Part1(DataType.Example, 21);
+
+    [Fact]
+    public override async Task Part1_Actual()
+        => await Invoke_Part1(DataType.Actual, 1807);
+
+    [Fact]
+    public override async Task Part2_Example()
+        => await Invoke_Part2(DataType.Example, 8);
+
+
+    [Fact]
+    public override async Task Part2_Actual()
+        => await Invoke_Part2(DataType.Actual, 480000);
+
+    private async Task Invoke_Part1(DataType dataType, int? expected = null)
+        => await Invoke(
+            Part.Two,
+            dataType,
+            forrest => forrest.GetVisibleTrees(RCVector.AllCardinal).Count(),
+            expected);
+
+    private async Task Invoke_Part2(DataType dataType, int? expected = null)
+        => await Invoke(
+            Part.Two,
+            dataType,
+            forrest => forrest.GetTreesVisibleFromTree(RCVector.AllCardinal).Max(tree => tree.CanSee.Aggregate(1, (cur, next) => cur * next.Trees.Count())),
+            expected);
+
+    private async Task Invoke(Part part, DataType dataType, Func<Forrest, int> sut, int? expected = null)
+    {
+        var data = await GetData(dataType);
+        var forrest = new Forrest(data);
+        var result = sut.Invoke(forrest);
+
+        WriteResult(part, result);
+
+        if (expected.HasValue)
+        {
+            result.Should().Be(expected);
+        }
+    }
+
+
+    private class RCVector
+    {
+        public static readonly North North = new();
+        public static readonly East East = new();
+        public static readonly South South = new();
+        public static readonly West West = new();
+        public static readonly RCVector[] AllCardinal = new RCVector[] { North, East, South, West };
+
+        public int Row { get; }
+        public int Column { get; }
+        public RCVector(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+
+    private class North : RCVector
+    {
+        public North() : base(-1, 0) { }
+    }
+    private class East : RCVector
+    {
+        public East() : base(0, 1) { }
+    }
+    private class South : RCVector
+    {
+        public South() : base(1, 0) { }
+    }
+    private class West : RCVector
+    {
+        public West() : base(0, -1) { }
+    }
+
+    private class Tree
+    {
+        public RCVector Position { get; }
+        public int Height { get; }
+
+        public Tree(int row, int column, int height)
+        {
+            Position = new RCVector(row, column);
+            Height = height;
+        }
+    }
+
+    private class Forrest : List<List<Tree>>
+    {
+        public int Width => this.FirstOrDefault()?.Count ?? 0;
+        public int Length => Count;
+
+        public Forrest(string[] treeMap)
+        {
+            for (var row = 0; row < treeMap.Length; row++)
+            {
+                Add(new List<Tree>());
+                var treeHeights = treeMap[row].ToCharArray().Select(c => int.Parse(c.ToString())).ToList();
+
+                for (var col = 0; col < treeHeights.Count; col++)
+                {
+                    this[row].Add(new Tree(row, col, treeHeights[col]));
+                }
+            }
+        }
+
+        public IEnumerable<Tree> GetVisibleTrees(params RCVector[] directions)
+        {
+            foreach (var row in this)
+            {
+                foreach (var tree in row)
+                {
+                    if (IsOnEdge(tree))
+                    {
+                        yield return tree;
+                        continue;
+                    }
+
+                    foreach (var direction in directions)
+                    {
+                        if (IsVisibleFrom(tree, direction))
+                        {
+                            yield return tree;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<(Tree Tree, IEnumerable<(RCVector Direction, IEnumerable<Tree> Trees)> CanSee)> GetTreesVisibleFromTree(params RCVector[] viewDirections)
+        {
+            foreach (var row in this)
+            {
+                foreach (var tree in row)
+                {
+                    var canSee = new List<(RCVector, IEnumerable<Tree>)>();
+                    foreach (var direction in viewDirections)
+                    {
+                        var treesVisibleByDirection = GetTreesVisibleFromTree(tree, direction).ToList();
+                        canSee.Add((direction, treesVisibleByDirection));
+                    }
+                    yield return (tree, canSee);
+                }
+            }
+        }
+
+        private IEnumerable<Tree> GetTreesVisibleFromTree(Tree tree, RCVector direction)
+        {
+            if (IsOnEdge(tree, direction))
+            {
+                yield break;
+            }
+
+            var otherTree = this[tree.Position.Row + direction.Row][tree.Position.Column + direction.Column];
+
+            while (otherTree != null)
+            {
+                if (otherTree.Height >= tree.Height)
+                {
+                    yield return tree;
+                    break;
+                }
+
+                yield return tree;
+                var otherRow = otherTree.Position.Row + direction.Row;
+                var otherCol = otherTree.Position.Column + direction.Column;
+                otherTree = this.ElementAtOrDefault(otherRow)?.ElementAtOrDefault(otherCol);
+            }
+        }
+
+        private bool IsVisibleFrom(Tree tree, RCVector direction)
+        {
+            var otherTree = this[tree.Position.Row + direction.Row][tree.Position.Column + direction.Column];
+            while (otherTree != null)
+            {
+                if (otherTree.Height >= tree.Height) return false;
+
+                var otherRow = otherTree.Position.Row + direction.Row;
+                var otherCol = otherTree.Position.Column + direction.Column;
+                otherTree = this.ElementAtOrDefault(otherRow)?.ElementAtOrDefault(otherCol);
+            }
+            return true;
+        }
+
+        private bool IsOnEdge(Tree tree)
+            => tree.Position.Row == 0 || tree.Position.Column == 0
+            || tree.Position.Row == Width - 1 || tree.Position.Column == Length - 1;
+
+        private bool IsOnEdge(Tree tree, RCVector direction) => direction switch
+        {
+            North => tree.Position.Row == 0,
+            East => tree.Position.Column == Width - 1,
+            South => tree.Position.Row == Length - 1,
+            West => tree.Position.Column == 0,
+            _ => throw new NotImplementedException("Direction not supported")
+        };
+    }
+}
